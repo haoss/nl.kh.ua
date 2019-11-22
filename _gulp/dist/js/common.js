a46a617ae2b42de5dccbde45cf8be4d807079d14
Dmitriy Khristoev <haossut@gmail.com>
2019-11-22 23:27:45 +0200

    new files

---
+++ _gulp/dist/js/common.js
@@ -0,0 +1,213 @@
+'use strict'
+
+// Document ready
+$(document).on('ready', function(){
+
+  // Magnific popup gallery
+  $('.gallery').each(function() {
+    $(this).magnificPopup({
+      delegate: '.gallery-item',
+      type: 'image',
+      gallery:{
+        enabled:true
+      },
+      zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+          // openerElement is the element on which popup was initialized, in this case its <a> tag
+          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+  });
+
+  // Magnific popup one image
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    mainClass: 'mfp-img-mobile',
+    image: {
+      verticalFit: true
+    }
+  });
+
+  // Magnific popup video
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+
+  $('.open-popup-link').magnificPopup({
+    type: 'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });
+
+  mobileNav();
+  phoneMask();
+
+  $("#tableDiamonds").tablesorter();
+
+  // Chrome Smooth Scroll
+  try {
+    $.browserSelector();
+    if($("html").hasClass("chrome")) {
+      $.smoothScroll();
+    }
+  } catch(err) {
+
+  };
+
+  // simpleForm version 2015-09-23 14:30 GMT +2
+  simpleForm('form.form-callback');
+});
+
+$(window).on('load', function() {
+  // $(".loader_inner").fadeOut();
+  $(".loader").delay(400).fadeOut("slow");
+});
+
+$(window).on('scroll', function() { });
+$(window).on('resize', function() {
+  var width = $(window).width();
+  var btn = $('.header__button');
+  var shadow = $('.body__shadow');
+  var nav = $('.header__nav');
+
+  if (width > 1199) {
+    btn.removeClass('is-active');
+    shadow.removeClass('is-active');
+    nav.removeClass('is-active');
+  }
+});
+
+/*
+version 2015-09-23 14:30 GMT +2
+*/
+function simpleForm(form, callback) {
+  $(document).on('submit', form, function(e){
+    e.preventDefault();
+    var formData = {};
+    var hasFile = false;
+    if ($(this).find('[type=file]').length < 1) {
+      formData = $(this).serialize();
+    }
+    else {
+      formData = new FormData();
+      $(this).find('[name]').each(function(){
+
+        switch($(this).prop('type')) {
+
+          case 'file':
+            if ($(this)[0]['files'].length > 0) {
+              formData.append($(this).prop('name'), $(this)[0]['files'][0]);
+              hasFile = true;
+            }
+            break;
+
+          case 'radio':
+          case 'checkbox':
+            if (!$(this).prop('checked')) {
+              break;
+            }
+            formData.append($(this).prop('name'), $(this).val().toString());
+            break;
+
+          default:
+            formData.append($(this).prop('name'), $(this).val().toString());
+            break;
+        }
+      });
+    }
+
+    $.ajax({
+      url: $(this).prop('action'),
+      data: formData,
+      type: 'POST',
+      contentType : hasFile ? 'multipart/form-data' : 'application/x-www-form-urlencoded',
+      cache       : false,
+      processData : false,
+      success: function(response) {
+        $(form).removeClass('ajax-waiting');
+        $(form).html($(response).find(form).html());
+
+        if (typeof callback === 'function') {
+          callback(response);
+        }
+      }
+    });
+
+    $(form).addClass('ajax-waiting');
+
+    return false;
+  });
+}
+
+function mobileNav() {
+  var btn = $('.header__button');
+  var shadow = $('.body__shadow');
+  var nav = $('.header__nav');
+
+  btn.on('click', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    btn.toggleClass('is-active');
+    shadow.toggleClass('is-active');
+    nav.toggleClass('is-active');
+  });
+
+  $(document).on('click', function(e){
+    if (btn.hasClass('is-active') && shadow.hasClass('is-active') && nav.hasClass('is-active')) {
+      btn.removeClass('is-active');
+      shadow.removeClass('is-active');
+      nav.removeClass('is-active');
+    }
+  });
+}
+
+;( function ( document, window, index )
+{
+	var inputs = document.querySelectorAll( '.inputfile' );
+	Array.prototype.forEach.call( inputs, function( input )
+	{
+		var label	 = input.nextElementSibling,
+			labelVal = label.innerHTML;
+
+		input.addEventListener( 'change', function( e )
+		{
+			var fileName = '';
+			if( this.files && this.files.length > 1 )
+				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+			else
+				fileName = e.target.value.split( '\\' ).pop();
+
+			if( fileName )
+				label.querySelector( 'span' ).innerHTML = fileName;
+			else
+				label.innerHTML = labelVal;
+		});
+
+		// Firefox bug fix
+		input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
+		input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+	});
+}( document, window, 0 ));
+
+function phoneMask(){
+	var phone = $('.phone-mask');
+	phone.each(function(){
+		$(this).mask("+38 (999) 999 99 99");
+	})
+}
